63e2c7eba59a26a30d9068d0d49ee089409f3965
Gabomou07 <gabriel.vegabalcazar@gmail.com>
2023-06-18 17:37:44 -0500

    controlador actualizar bien hecho

--- src/main/java/cibertec/edu/pe/projectowebfinal/controller/CategoriasController.java
+++ src/main/java/cibertec/edu/pe/projectowebfinal/controller/CategoriasController.java
@@ -1,9 +1,9 @@
 package cibertec.edu.pe.projectowebfinal.controller;
 
 import cibertec.edu.pe.projectowebfinal.model.Categorias;
-import cibertec.edu.pe.projectowebfinal.model.Producto;
 import cibertec.edu.pe.projectowebfinal.service.CategoriaService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -21,6 +21,7 @@ public class CategoriasController {
         model.addAttribute("categorias",categorias);
         return "Categoria/ListarCategorias";
     }
+    //q
 
     @PostMapping("/saveCategoria")
     public String saveCategoria(@ModelAttribute("categorias") Categorias categorias){
@@ -29,8 +30,22 @@ public class CategoriasController {
     }
 
     @GetMapping("/deleteCategoria/{idtipo}")
-    public String deleteCategoria(@PathVariable(value = "idtipo") Integer idtipo){
+    public String deleteCategoria(@PathVariable(value = "idtipo") int idtipo){
         this.categoriaService.deleteCategoryById(idtipo);
         return "redirect:/Categoria/listarCategorias";
+    }
+    
+    @GetMapping("/updateCategoria/{idtipo}")
+    public ResponseEntity<Categorias> updateCategoria(@PathVariable(value = "idtipo") int idtipo) {
+        // Get the product from the service
+        Categorias categorias = categoriaService.getCategoriaById(idtipo);
+
+        if (categorias == null) {
+            // Return an appropriate response if the product is not found
+            return ResponseEntity.notFound().build();
+        }
+
+        // Return the product in the response body
+        return ResponseEntity.ok(categorias);
     }
 }
